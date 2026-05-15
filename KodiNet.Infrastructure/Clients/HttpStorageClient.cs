e32e08f337c41e7f8e683970db6ae38417538043
Mathias BENNER <mathias.benner@jmjautomobiles.com>
2026-05-15 14:28:16 +0200

    r

--- KodiNet.Infrastructure/Clients/HttpStorageClient.cs
+++ KodiNet.Infrastructure/Clients/HttpStorageClient.cs
@@ -198,9 +198,9 @@ internal sealed class ProgressStream(Stream inner, IProgress<double> progress) :
         return read;
     }
 
-    public override int Read(Span<byte> buffe)
+    public override int Read(Span<byte> buffer)
     {
-        var read = inner.Read(buffe);
+        var read = inner.Read(buffer);
         Report(read);
         return read;
     }
